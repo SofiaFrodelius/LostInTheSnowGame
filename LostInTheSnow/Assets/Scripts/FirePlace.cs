d75b1c9e3ed7d55c4333c2ecfc9770c2c140fc30
Lambipapp <lars.johan.urban.laven@gmail.com>
2018-05-14 11:41:30 +0200

    Can light Fire bruthas

---
+++ LostInTheSnow/Assets/Scripts/FirePlace.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FirePlace : MonoBehaviour, IInteractible
+{
+
+    [SerializeField] private GameObject particleSys;
+    [SerializeField] private Transform particlePos;
+    [SerializeField] private StudioEventEmitter fireSound;
+    [SerializeField] private float timeToLightFire;
+    private bool hasLitten = false;
+
+    public void Interact()
+    {
+       
+        
+        if(!hasLitten)
+            ToggleFire();
+    }
+    void ToggleFire()
+    {
+        hasLitten = true;
+
+        fireSound.gameObject.SetActive(true);        
+        StartCoroutine(fireStarter(timeToLightFire));
+    }
+
+    IEnumerator fireStarter(float time)
+    {
+        yield return new WaitForSeconds( time);
+        
+        GameObject tmpPS = Instantiate(particleSys, particlePos);
+        gameObject.GetComponent<FirePlace>().enabled = false;
+    }
+}
