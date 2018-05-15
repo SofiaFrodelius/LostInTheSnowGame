8eddd2897f98eb41c4f7ae942f466e73f945c0f7
Carl-Olof <olav685@gmail.com>
2018-05-15 15:11:05 +0200

    so much stuff

---
+++ LostInTheSnow/Assets/Dog/Scripts/Actions/WaitForFetch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForFetch : DogAction
+{
+	public WaitForFetch(Dog d) : base(d){
+		importance = Importance.LOW;
+	}
+	public override void StartAction(){
+		isDone = false;
+		animator.SetBool ("WaitForFetch", true);
+	}
+	public override void UpdateAction(){
+	}
+	public override void EndAction(){
+		animator.SetBool ("WaitForFetch", false);
+		isDone = true;
+	}
+}
