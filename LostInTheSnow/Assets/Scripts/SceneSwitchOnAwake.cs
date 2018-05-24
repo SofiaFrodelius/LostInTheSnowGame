2cac9574b5d0072f18d65cffdd6e63400fcc6c93
HolmErik <erikholm@live.com>
2018-05-24 10:59:35 +0200

    sceneswitch on awake

---
+++ LostInTheSnow/Assets/Scripts/SceneSwitchOnAwake.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitchOnAwake : MonoBehaviour
+{
+    [SerializeField] private SceneSwitchScript sceneSwitcher;
+    [SerializeField] private int sceneIndex;
+
+
+    private void Awake()
+    {
+        sceneSwitcher.ActivateSceneSwitch(sceneIndex);
+    }
+}
