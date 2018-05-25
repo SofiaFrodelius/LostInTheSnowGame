9f822d0033c4c8a776cbf4474b104dea7eb813de
HolmErik <erikholm@live.com>
2018-05-25 10:55:34 +0200

    interact cutscene trigger

---
+++ LostInTheSnow/Assets/Scripts/InteractCutscene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractCutscene : MonoBehaviour, IInteractible
+{
+    [SerializeField] private GameObject cutsceneToTrigger;
+    [SerializeField] private GameObject player;
+
+
+    public void AlternateInteract()
+    {
+
+    }
+
+    public void Interact()
+    {
+        Camera.main.gameObject.GetComponent<CameraController>().CutsceneLock = true;
+        player.GetComponent<CharacterMovement>().CutsceneLock = true;
+        GameObject temp = Instantiate(cutsceneToTrigger);
+        player.transform.parent = temp.transform;
+        temp.SetActive(true);
+    }
+}
