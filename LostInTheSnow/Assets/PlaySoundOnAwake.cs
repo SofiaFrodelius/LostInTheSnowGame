f4fd919766d1400a951eabff29c7fb052b409641
HolmErik <erikholm@live.com>
2018-05-25 10:19:41 +0200

    playsoundonawake för door close ljud

---
+++ LostInTheSnow/Assets/PlaySoundOnAwake.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class PlaySoundOnAwake : MonoBehaviour
+{
+    [SerializeField] private StudioEventEmitter see;
+
+
+
+    private void Awake()
+    {
+        see.Play();
+    }
+}
