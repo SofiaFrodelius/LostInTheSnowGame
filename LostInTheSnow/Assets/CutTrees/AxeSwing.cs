18a6b91047e41386fd7146420c376284786d3f1e
Carl-Olof <olav685@gmail.com>
2018-05-28 16:32:03 +0200

    act 1 and woodchop

---
+++ LostInTheSnow/Assets/CutTrees/AxeSwing.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeSwing : MonoBehaviour, IUsable
+{
+    bool InspectBool = false;
+    bool TreeChopBool = false;
+    [SerializeField] private GameObject chopParticles;
+    [SerializeField] private Transform particlePos;
+    Animator anim;
+    private bool isChopping = false;
+    private int counter = 0;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void Use(ItemHand ih)
+    {
+        SwingAxe();
+    }
+    void SwingAxe()
+    {
+        if (!isChopping)
+        {
+            anim.SetTrigger("AxeChop");
+            isChopping = true;
+        }
+    }
+    private void Update()
+    {
+        if (isChopping)
+        {
+            counter++;
+            if (counter >= 275)
+            {
+                counter = 0;
+                isChopping = false;
+            }
+        }
+    }
+    public void AxeHit()
+    {
+        Debug.Log("Instansierar partikelsystem");
+        Instantiate(chopParticles, particlePos);
+    }
+
+    public bool IsChopping
+    {
+        get { return isChopping; }
+    }
+}
---
+++ LostInTheSnow/Assets/CutTrees/CuttableTree.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttableTree : MonoBehaviour, IInteractible
+{
+    private int hitPoints = 3;
+    private bool activated = false;
+    private bool chopFinished = true;
+    private GameObject playerObj;
+    private CharacterMovement charMove = null;
+    GameObject activeItem = null;
+    [SerializeField] private GameObject firewood;
+
+    public void AlternateInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact()
+    {
+        playerObj = GameObject.FindWithTag("Player");
+        if (playerObj != null)
+            charMove = playerObj.GetComponent<CharacterMovement>();
+        if (charMove != null)
+        {
+            ItemHand itemHand = Camera.main.GetComponentInChildren<ItemHand>();
+            if (itemHand != null)
+                activeItem = itemHand.ActiveItem;
+            if (activeItem != null)
+            {
+                if (activeItem.tag == "Axe")
+                {
+                    activated = true;
+                    float hAngle;
+                    Vector2 deltaVector = new Vector2(transform.position.x - playerObj.transform.position.x, transform.position.z - playerObj.transform.position.z);
+                    hAngle = Mathf.Atan2(deltaVector.x, deltaVector.y);
+                    hAngle *= 360;
+                    hAngle /= Mathf.PI * 2;
+                    hAngle += 1.8f;
+                    while (hAngle < 0)
+                        hAngle += 360;
+                    while (hAngle >= 360)
+                        hAngle -= 360;
+                    Vector2 targetLook = new Vector2(hAngle, 0f);
+                    Vector3 cuttingposition = new Vector3(-deltaVector.x, 0, -deltaVector.y);
+                    cuttingposition.Normalize();
+                    float cuttingDistance = 1.9f;
+                    charMove.ForceMovement(transform.position + new Vector3(cuttingposition.x * cuttingDistance, charMove.transform.position.y - transform.position.y,
+                        cuttingposition.z * cuttingDistance), targetLook, false);
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (activated)
+        {
+            if (charMove != null)
+            {
+                if (!charMove.GetForcedMove())
+                {
+                    AxeSwing axeSwing = activeItem.GetComponent<AxeSwing>();
+                    if (axeSwing != null)
+                    {
+                        // Play animation here
+                        ItemHand itemHand = Camera.main.GetComponentInChildren<ItemHand>();
+                        if (itemHand != null && chopFinished)
+                        {
+                            axeSwing.Use(itemHand);
+                            chopFinished = false;
+                        }
+                        if (!chopFinished)
+                        {
+                            if (!axeSwing.IsChopping)
+                            {
+                                chopFinished = true;
+                            }
+                        }
+                        if (chopFinished)
+                        {
+                            activated = false;
+                            charMove.CutsceneRelease();
+                            hitPoints--;
+                            if (hitPoints <= 0)
+                            {
+                                // Tree dying stuff goes here
+                                for (int i = 0; i < 10; i++)
+                                {
+                                    Instantiate(firewood, transform.position + new Vector3(0, 0.2f + (0.5f * i), 0), Quaternion.LookRotation(Vector3.up));
+                                }
+                                gameObject.SetActive(false);
+                            }
+                        }
+                    }
+                    else Debug.Log("axeSwing is null");
+                }
+            }
+            else Debug.Log("charMove is null");
+        }
+    }
+}
