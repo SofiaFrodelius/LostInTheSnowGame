6dd0202886ebd74f875fb09fc323253591e94206
Lambipapp <lars.johan.urban.laven@gmail.com>
2018-05-29 10:27:42 +0200

    lite pill

---
+++ LostInTheSnow/Assets/Scripts/particleKiller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleKiller : MonoBehaviour {
+    List<ParticleSystem> ps;
+    int psDone = 0;
+
+	// Use this for initialization
+	void Start () {
+        ps = new List<ParticleSystem>(GetComponentsInChildren<ParticleSystem>());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(ParticleSystem p in ps)
+        {
+            if (p.isStopped)
+            {
+                psDone++;
+            }
+            else
+            {
+                psDone = 0;
+            }
+        }
+        if(psDone == ps.Count)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
