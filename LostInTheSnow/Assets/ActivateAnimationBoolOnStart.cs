d7d135fa8bb6b33af8c373da4678e4f241b999bb
Lambipapp <lars.johan.urban.laven@gmail.com>
2018-05-30 10:45:57 +0200

    hund sova

---
+++ LostInTheSnow/Assets/ActivateAnimationBoolOnStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateAnimationBoolOnStart : MonoBehaviour {
+    [SerializeField] private string booleanName;
+    [SerializeField] private bool condition;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Animator>().SetBool(booleanName, condition);
+	}
+
+}
